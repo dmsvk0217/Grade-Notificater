8cab36ab2d68a4107e437c998219454461aa7de6
dmsvk0217 <dmsvk0217@gmail.com>
2023-12-25 22:39:07 +0900

    client build

---
+++ client/build/precache-manifest.8b0d76e899c465f7cae59902b2f99d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6c9cb5238d6ab76418db7d3306968f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46d61a3869e03e3fc686",
+    "url": "/static/css/main.8e498ca7.chunk.css"
+  },
+  {
+    "revision": "e7c6f5ccc1a718ec81da",
+    "url": "/static/js/2.d139f279.chunk.js"
+  },
+  {
+    "revision": "5a4886f14037d20c58d1df2b9580f715",
+    "url": "/static/js/2.d139f279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25cd98592a70d5a6a4a",
+    "url": "/static/js/3.d989cbb4.chunk.js"
+  },
+  {
+    "revision": "46d61a3869e03e3fc686",
+    "url": "/static/js/main.a4e2554e.chunk.js"
+  },
+  {
+    "revision": "27e4e67c0d7457837ed8",
+    "url": "/static/js/runtime-main.fb8ff8a9.js"
+  }
+]);
