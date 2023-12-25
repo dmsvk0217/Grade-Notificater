de8c99a2a0987d3ac720203fd70525143ad9d6cf
dmsvk0217 <dmsvk0217@gmail.com>
2023-12-25 22:51:10 +0900

    [client] server url 수정

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const serverURL =
   process.env.NODE_ENV === "production"
-    ? "http://ec2-43-200-184-150.ap-northeast-2.compute.amazonaws.com:4000/"
-    : "http://localhost:4000/";
+    ? "http://ec2-43-203-106-55.ap-northeast-2.compute.amazonaws.com:4000"
+    : "http://localhost:4000";
 
 module.exports = serverURL;
